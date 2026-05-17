bda1e8a2482517437e22f29e533fef3d796acc72
liinnea <linneaolivia08@gmail.com>
2026-05-17 18:17:21 +0200

    info färdig
    är man typ färdig eller. är ju inte helt nöjd med mina container så det ska fixas imorgon... även göra det lite snyggare kansek

---
+++ slutprojekt/slutprojekt flm och serie/slutprojekt flm och serie/slutprojekt/index.js
@@ -0,0 +1,151 @@
+// menuButton
+
+function toggleMenu(){
+
+    document.querySelector("nav").classList.toggle("hidden");
+
+}
+
+
+// loop för card
+
+ReactDOM.createRoot(document.querySelector("main")).render(<App></App>)
+
+function App(){
+
+    return(
+
+        <div className="app">
+
+            <Products></Products>
+
+        </div>
+
+    )
+
+}
+
+function Products(){
+
+    const [products, setProducts] = React.useState([])
+
+    // kallar på getData exakt en (1) gång
+
+    React.useEffect(()=>{getData()}, [])
+
+    async function getData(){
+
+        const res = await fetch("prod.json");
+        const data = await res.json();
+
+        setProducts(data);
+        
+    }    
+
+    return(
+
+        <div className="products">
+
+            {products.map(p => <Prod key={p.id} prod={p} ></Prod>)}
+
+        </div>
+
+    )
+
+}
+
+function Prod({prod}){
+
+    const [info, setInfo] = React.useState(false);
+
+    function toggleInfo(){
+
+        setInfo(p=>!p)
+
+    }
+
+    function Slideshow({images}){
+
+    const [index, setIndex] = React.useState(0);
+
+    React.useEffect(()=>{
+
+        const interval = setInterval(function(){
+
+            setIndex(prev => {
+            /*   return  prev == images.length - 1 ? 0 : prev + 1 */
+
+                if(images.length-1 == prev) return 0;
+
+                return prev + 1
+
+
+        });
+
+        }, 3000);
+
+        return ()=> clearInterval(interval);
+
+    }, [images]);
+
+    return(
+
+        <div className="slideshow">
+
+            <img 
+                className="info-img"
+                src={images[index]} 
+                alt="movie image"
+            />
+
+        </div>
+
+    )
+
+}
+
+    return(
+
+        <div className="outerCard" >
+
+            <div className="innerCard">
+
+                <div className="imgBox" >
+
+                    <img src={prod.img} alt="no_image" ></img>
+
+                </div>
+
+                <div className="textBox" >
+                    <h2 className="title" >{prod.title}</h2>
+                    <p className="description" > {prod.description} </p>
+                    <button onClick={toggleInfo} className="btnBox" >LÄS MER</button>
+                </div>
+
+                {info ?    <div className="info">
+
+
+                <div className="infoCard">
+
+                    <Slideshow images={prod.info.img} />
+
+                </div>
+
+                    
+                    
+                    <div className="descBox" >
+                        <h2>{prod.title}</h2>
+                        <p className="info-desc" >{prod.info.description}</p>
+                        <button onClick={toggleInfo} className="btnBox" >STÄNG</button>
+                    </div>
+
+                </div> : ""}
+            
+
+            </div>
+
+        </div>
+
+    )
+
+}
